87b7142c2d65dc178a0f2daeb0fe4050668e62c5
qlibcrist <bcrist@qualitylogic.com>
2023-01-23 10:14:20 -0700

    Adds some css and fixes GET call in checkboxes

--- src/components/UI/checkboxes.js
+++ src/components/UI/checkboxes.js
@@ -2,17 +2,13 @@ import { useEffect, useState } from "react"
 import { v4 } from "uuid"
 
 export const Checkboxes = (props) => {
-  const values = props.values
-  const [getData, setGetData] = useState({})
+  const [getData, setGetData] = useState([])
 
   // GET request w/ hooks; calls when component loads
   useEffect(() => {
-    console.log('Fetching...')
     fetch('/api/checkboxes')
       .then(res => res.json().then(data => {setGetData(data[0].data)}))
   }, [])
-
-  console.log(getData)
 
   return (
     <div className="CheckboxWrapper">
